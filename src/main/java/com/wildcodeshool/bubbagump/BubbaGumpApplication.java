0276d99704a5e6cb0bce08b6017c0d3698166a71
ganout <morgan.bourdillel@gmail.com>
2020-07-22 13:09:54 +0200

    init

---
+++ src/main/java/com/wildcodeshool/bubbagump/BubbaGumpApplication.java
@@ -0,0 +1,13 @@
+package com.wildcodeshool.bubbagump;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BubbaGumpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BubbaGumpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wildcodeshool/bubbagump/BubbaGumpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wildcodeshool.bubbagump;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubbaGumpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
